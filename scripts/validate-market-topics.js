d26fdfa5c33d1f72adab57368f9cdcec44bde65c
howard891014-a11y <howard891014@gmail.com>
2026-09-25 13:51:27 +0800

    Add files via upload

--- scripts/validate-market-topics.js
+++ scripts/validate-market-topics.js
@@ -22,6 +22,14 @@ for(const [code,name] of [['8027','鈦昇'],['6664','群翊'],['8064','東捷'],
   assert(links.some(x=>x.id==='foplp'),`${code} ${name} should receive FOPLP market overlay`);
 }
 assert(marketTopicLinks([],'鴻勁','7769').some(x=>x.id==='cpo_silicon_photonics'),'鴻勁 should receive CPO/矽光子 market overlay');
+for(const [code,name] of [['2330','台積電'],['6451','訊芯-KY'],['3711','日月光投控'],['3450','聯鈞'],['4977','眾達-KY']]){
+  const links=marketTopicLinks([],name,code);
+  assert(links.some(x=>x.id==='cpo_silicon_photonics'),`${code} ${name} should receive CPO/矽光子 market-reference overlay`);
+}
+for(const [code,name] of [['2314','台揚'],['2485','兆赫'],['7812','稜研科技*-創']]){
+  const links=marketTopicLinks([],name,code);
+  assert(links.some(x=>x.id==='leo_satellite'),`${code} ${name} should receive 低軌衛星 market-reference overlay`);
+}
 const thermal=marketTopicLinks([{id:'liquid_cooling',name:'液冷散熱',importance:'core',origin:'test'}],'奇鋐');
 assert(thermal.some(x=>x.id==='thermal'&&x.importance==='core'),'liquid cooling should consolidate to 散熱');
 const bbu=marketTopicLinks([],'AES-KY');
